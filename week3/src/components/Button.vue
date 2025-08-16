8a21e135046a02d14f6973fa89b14ceb1bdd4ac4
Xuqing Lin <xlin0057@student.monash.edu>
2025-08-16 14:24:20 +1000

    recover the webpage before challenges activities

--- week3/src/components/Button.vue
+++ week3/src/components/Button.vue
@@ -1,98 +1,5 @@
-<!-- // challenge 1
-<template>
-  <button
-    :class="buttonClass"
-    :disabled="disabled"
-    @click="handleClick"
-  >
-    {{ label }}
-  </button>
-</template>
-
 <script setup>
 import { computed } from 'vue'
-
-// defineProps for button properties
-const props = defineProps({
-  label: {
-    type: String,
-    required: true
-  },
-  color: {
-    type: String,
-    default: 'primary' // default color
-  },
-  size: {
-    type: String,
-    default: 'medium', // default size
-    validator: (val) => ['small', 'medium', 'large'].includes(val)
-  },
-  disabled: {
-    type: Boolean,
-    default: false
-  },
-  style: {
-    type: String,
-    default: 'filled', // default style
-    validator: (val) => ['outlined', 'filled'].includes(val)
-  }
-})
-
-// defineEmits for button click event
-const emit = defineEmits(['click'])
-
-// handleClick function to emit click event
-const handleClick = (event) => {
-  if (!props.disabled) {
-    emit('click', event)
-  }
-}
-
-// computed property to determine the button class based on props
-const buttonClass = computed(() => {
-  let classes = ['btn'] // Bootstrap base class
-
-  // determine color class
-  if (props.style === 'outlined') {
-    classes.push(`btn-outline-${props.color}`)
-  } else {
-    classes.push(`btn-${props.color}`)
-  }
-
-  // determine size class
-  if (props.size === 'small') {
-    classes.push('btn-sm')
-  } else if (props.size === 'large') {
-    classes.push('btn-lg')
-  }
-
-  return classes
-})
-</script> -->
-
-
-<!-- // challenge 2 -->
-<template>
-  <button
-    :style="buttonStyle"
-    :disabled="disabled"
-    @click="handleClick"
-  >
-    {{ label }}
-  </button>
-  <div style="padding:24px; display: grid; gap: 12px; max-width: 360px;">
-    <Button label="Prime Button" icon="pi pi-check" />
-    <InputText v-model="name" placeholder="Your name" />
-  </div>
-</template>
-
-<script setup>
-import { computed } from 'vue'
-import { ref } from 'vue'
-import Button from 'primevue/button'
-import InputText from 'primevue/inputtext'
-
-const name = ref('')
 
 const props = defineProps({
   label: { type: String, required: true },
@@ -130,7 +37,6 @@ const buttonStyle = computed(() => {
     large: '12px 24px'
   }
 
-  // select size based on props
   if (props.style === 'outlined') {
     return {
       backgroundColor: 'transparent',
