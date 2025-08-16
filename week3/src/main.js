3d49577dab52ef7ad6c999e18342f37568ac430f
Xuqing Lin <xlin0057@student.monash.edu>
2025-08-16 10:23:57 +1000

    activity 4

--- week3/src/main.js
+++ week3/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './style.css'
+// import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App).mount('#app')
