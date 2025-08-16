eec6fb9533d6505237164864f272b326ff08280e
Xuqing Lin <xlin0057@student.monash.edu>
2025-08-16 15:00:01 +1000

    week 4 acttivity 3.1 Remove all HTML validations

--- week3/src/components/Form.vue
+++ week3/src/components/Form.vue
@@ -13,7 +13,7 @@
                 type="text"
                 class="form-control"
                 id="username"
-                required v-model="formData.username"
+                v-model="formData.username"
               />
             </div>
             <div class="col-md-6">
@@ -22,9 +22,6 @@
                 type="password"
                 class="form-control"
                 id="password"
-                required
-                minlength="4"
-                maxlength="10"
                 v-model="formData.password"
               />
             </div>
@@ -38,7 +35,6 @@
                   type="checkbox"
                   class="form-check-input"
                   id="isAustralian"
-                  required
                   v-model="formData.isAustralian"
                 />
                 <label class="form-check-label" for="isAustralian">
@@ -52,7 +48,6 @@
                 class="form-select"
                 id="gender"
                 v-model="formData.gender"
-                required
               >
                 <option value="">Select</option>
                 <option value="male">Male</option>
@@ -70,9 +65,6 @@
               id="reason"
               rows="3"
               v-model="formData.reason"
-              required
-              minlength="10"
-              maxlength="200"
             ></textarea>
           </div>
 
