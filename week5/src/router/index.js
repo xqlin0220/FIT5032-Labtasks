0eb61b6c097f8269c99407aee5c62014e0d31c6a
Xuqing Lin <xlin0057@student.monash.edu>
2025-08-26 17:18:14 +1000

    update App.vue

--- week5/src/router/index.js
+++ week5/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import LoginView from '../views/LoginView.vue'
-import { isAuthenticated } from '../stores/auth'
+import { isAuthenticated } from '../stores/auth.js'
 
 const routes = [
   {
