d73c0fc4b2cabe20be905ebaaf85d9a1e72dd1f9
Xuqing Lin <xlin0057@student.monash.edu>
2025-08-26 16:58:15 +1000

    activities 6.1 Redirecting Unauthenticated Users

--- week5/src/router/index.js
+++ week5/src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import LoginView from '../views/LoginView.vue'
-import AccessDeniedView from '../views/AccessDeniedView.vue'
 import { isAuthenticated } from '../stores/auth'
 
 const routes = [
@@ -14,14 +13,14 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    component: AboutView
+    component: AboutView,
+    meta: { requiresAuth: true}
   },
   {
     path: '/login',
     name: 'Login',
     component: LoginView
   },
-  {path: '/access-denied', name: 'AccessDenied', component: AccessDeniedView },
 ]
 
 const router = createRouter({
@@ -30,7 +29,7 @@ const router = createRouter({
 })
 router.beforeEach((to, from, next) => {
   if (to.meta.requiresAuth && !isAuthenticated.value) {
-    next({ name: 'AccessDenied', query: { redirect: to.fullPath } })
+    next({ name: 'Login', query: { redirect: to.fullPath } })
   } else {
     next()
   }
