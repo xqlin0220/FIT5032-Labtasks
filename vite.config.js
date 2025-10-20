ca1b374042b8a4922c89dc573a6fc186584fb63b
Xuqing Lin <xlin0057@student.monash.edu>
2025-10-20 14:11:52 +1100

    trigger deploy to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,10 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)), 
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  base: process.env.NODE_ENV === 'production'
+    ? '/FIT5032-Labtasks/' 
+    : '/',         
 })
